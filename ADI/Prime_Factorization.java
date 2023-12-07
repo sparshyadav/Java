1ded2185d86f22ae1898aae4cdff20977bb20bf5
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-12-07 19:05:49 +0530

    Prime Factorization Method 2

--- ADI/Prime_Factorization.java
+++ ADI/Prime_Factorization.java
@@ -4,11 +4,24 @@ public class Prime_Factorization {
     static ArrayList<Integer> primeFactors(int n) {
         ArrayList<Integer> factors = new ArrayList<>();
 
-        for (int i = 2; i <= n; i++) {
+        // Method 1
+        // for (int i = 2; i <= n; i++) {
+        // while (n % i == 0) {
+        // factors.add(i);
+        // n /= i;
+        // }
+        // }
+
+        // Method 2
+        for (int i = 2; i * i <= n; i++) {
             while (n % i == 0) {
                 factors.add(i);
                 n /= i;
             }
+        }
+
+        if (n != 1) {
+            factors.add(n);
         }
 
         return factors;
